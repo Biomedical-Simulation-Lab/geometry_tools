e16ff775b0177010a9021401d4dac920769a9660
Anna Haley <anna.lk.haley@gmail.com>
2023-07-12 13:16:49 -0400

    Revert "scripts consistency"
    This reverts commit 203b5b549ff19fe8fd68ceee8bcf271df0180d68.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='geometry_tools',
     version='0.2dev',
     packages=['geometry_tools',],
-    scripts=['geometry_tools/scripts/surface_prep.py',
+    scripts=['geometry_tools/scripts//surface_prep.py',
     		'geometry_tools/scripts/surface_process.py',
     		'geometry_tools/scripts/volume_meshing.py',
     		'geometry_tools/scripts/meshquality.py',
