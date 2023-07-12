1a1b6510c269ce5b28d67387079d401b01705e3f
Anna Haley <anna.lk.haley@gmail.com>
2023-07-12 13:21:48 -0400

    added FrenetTangent to attributes

--- geometry_tools/scripts/centerlines_fixed.py
+++ geometry_tools/scripts/centerlines_fixed.py
@@ -38,6 +38,7 @@ def make_cl(prep_dir, case_name):
         #centerlines = vmtk.centerline_geometry(centerlines)
         #m.centerlines.save(out_dir/(case_name+'_centerline.vtp'))
         #m.centerlines = vmtk.centerlines_smooth(m.centerlines, iterations=1, sm_factor=0.1)
+        centerlines = vmtk.centerline_geometry(centerlines)
         centerlines.save(out_dir/(case_name+'_centerline_graph.vtp'))
         graph.save(out_dir/(case_name+'_graph.vtp'))
         
